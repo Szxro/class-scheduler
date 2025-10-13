b075009c57e8acba1bc0b0c85f721e680b103567
Sebastian <svargas@isf.do>
2025-10-13 16:25:58 -0400

    feat(navigation): add NavHost to the MainActivity class"

--- app/src/main/java/com/example/classscheduler/MainActivity.kt
+++ app/src/main/java/com/example/classscheduler/MainActivity.kt
@@ -5,9 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.classscheduler.ui.signin.SignInRoute
+import com.example.classscheduler.ui.signin.SignInScreen
 import com.example.classscheduler.ui.theme.ClassSchedulerTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -17,9 +23,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController();
             ClassSchedulerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Text("Helo World");
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                    NavHost(navController, startDestination = SignInRoute, modifier = Modifier.padding(innerPadding)){
+                        composable<SignInRoute>{
+                            SignInScreen(
+                                openHomeScreen = {},
+                                openSignUpScreen = {}
+                            )
+                        }
+                    }
                 }
             }
         }
