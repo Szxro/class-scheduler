5097f91c06b4cfb050ff7ee6541268caf7e8da74
Sebastian <svargas@isf.do>
2025-10-17 11:44:14 -0400

    refactor(domain): change function return type from Unit to Result<User>

--- app/src/main/java/com/example/classscheduler/domain/interfaces/AuthRepository.kt
+++ app/src/main/java/com/example/classscheduler/domain/interfaces/AuthRepository.kt
@@ -1,7 +1,10 @@
 package com.example.classscheduler.domain.interfaces
 
+import com.example.classscheduler.domain.models.User
+import com.example.classscheduler.domain.primitives.Result
+
 interface AuthRepository {
     suspend fun signUp(email: String, password: String): Unit;
-    suspend fun signIn(email: String, password: String): Unit;
+    suspend fun signIn(email: String, password: String): Result<User>;
     fun signOut();
 }
