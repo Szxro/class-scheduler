715620d4dafe580692504b95594da90ff8256efc
Sebastian <svargas@isf.do>
2025-10-20 16:05:59 -0400

    refactor(core): implement destination param to the navigate event

--- app/src/main/java/com/example/classscheduler/core/ui/UiEvent.kt
+++ app/src/main/java/com/example/classscheduler/core/ui/UiEvent.kt
@@ -1,7 +1,7 @@
 package com.example.classscheduler.core.ui
 
 sealed interface UiEvent{
-    data class Navigate(val args: Any? = null) : UiEvent;
+    data class Navigate(val destination: Screen, val args: Any? = null) : UiEvent;
 
     data class ShowSnackBar(val message: UiText) : UiEvent;
 }
