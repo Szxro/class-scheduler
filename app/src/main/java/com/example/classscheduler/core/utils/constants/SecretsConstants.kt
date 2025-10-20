ef38ed5d2ba4771cc4409cfef302a8ce964fcef5
Sebastian <svargas@isf.do>
2025-10-20 15:56:31 -0400

    feat(core): implement secrets constants

---
+++ app/src/main/java/com/example/classscheduler/core/utils/constants/SecretsConstants.kt
@@ -0,0 +1,8 @@
+package com.example.classscheduler.core.utils.constants
+
+import com.example.classscheduler.BuildConfig;
+
+object SecretsConstants{
+    const val WEB_CLIENT_ID = BuildConfig.WEB_CLIENT_ID;
+
+}
