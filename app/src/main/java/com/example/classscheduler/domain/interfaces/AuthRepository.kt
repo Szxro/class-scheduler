5482f5c5117e6dc381fc7013826d22df305bae25
Sebastian <svargas@isf.do>
2025-10-20 16:14:03 -0400

    refactor(domain): update the auth repository interface accordingly

--- app/src/main/java/com/example/classscheduler/domain/interfaces/AuthRepository.kt
+++ app/src/main/java/com/example/classscheduler/domain/interfaces/AuthRepository.kt
@@ -1,10 +1,15 @@
 package com.example.classscheduler.domain.interfaces
 
+import android.content.Context
 import com.example.classscheduler.domain.models.User
 import com.example.classscheduler.domain.primitives.Result
 
 interface AuthRepository {
-    suspend fun signUp(email: String, password: String): Unit;
-    suspend fun signIn(email: String, password: String): Result<User>;
-    fun signOut();
+    suspend fun signInWithEmailAndPassword(email: String, password: String): Result<User>;
+
+    suspend fun signInWithGoogle(context: Context): Result<User>;
+
+    suspend fun signUpWithEmailAndPassword(email: String, password: String): Result<User>;
+
+    suspend fun signOut(): Result<Nothing>;
 }
