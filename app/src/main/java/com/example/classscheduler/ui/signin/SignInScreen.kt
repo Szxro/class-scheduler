121ca15f451d9396b2e1f46b9854a122c69a5b15
Sebastian <svargas@isf.do>
2025-10-20 16:22:13 -0400

    feat(ui): implement google sign in flow, update intents and update how the navigate event is handled

--- app/src/main/java/com/example/classscheduler/ui/signin/SignInScreen.kt
+++ app/src/main/java/com/example/classscheduler/ui/signin/SignInScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -29,15 +30,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -46,9 +51,12 @@ import kotlinx.serialization.Serializable
 import com.example.classscheduler.R;
 import com.example.classscheduler.core.ui.UiEvent
 import com.example.classscheduler.core.ui.UiText
+import com.example.classscheduler.ui.signin.SignInIntent
+import com.example.classscheduler.core.ui.Screen
 import com.example.classscheduler.core.utils.ext.ObserveEventsAs
 import com.example.classscheduler.domain.models.User
 import com.example.classscheduler.ui.theme.DarkBlue
+import hilt_aggregated_deps._com_example_classscheduler_di_ClassSchedulerHiltApp_GeneratedInjector
 
 @Serializable
 object SignInRoute
@@ -66,11 +74,15 @@ fun SignInScreen(
 ):Unit{
     val state by signInViewModel.state.collectAsStateWithLifecycle();
 
+    val currentContext = LocalContext.current;
+
     signInViewModel.events.ObserveEventsAs { event ->
         when(event){
             is UiEvent.Navigate-> {
-                event.args?.let { user ->
-                    openHomeScreen(user as User);
+                when(event.destination){
+                    Screen.Home -> openHomeScreen(event.args!! as User)
+                    Screen.SignUp -> openSignUpScreen()
+                    else -> Unit
                 }
             }
             is UiEvent.ShowSnackBar -> showErrorSnackBar(event.message)
@@ -82,8 +94,9 @@ fun SignInScreen(
         onEmailChange = { email -> signInViewModel.onIntent(SignInIntent.OnEmailChange(email))},
         onPasswordChange = { password -> signInViewModel.onIntent(SignInIntent.OnPasswordChange(password))},
         onPasswordVisibilityChange = { signInViewModel.onIntent(SignInIntent.OnPasswordVisibilityChange) },
-        onSignUpButtonClicked = { signInViewModel.onIntent(SignInIntent.OnSignUp(openSignUpScreen))},
-        onSignInButtonClicked = { signInViewModel.onIntent(SignInIntent.OnSignIn) },
+        onSignUpButtonClicked = { signInViewModel.onIntent(SignInIntent.OnNavigateToSignUp)},
+        onSignInButtonClicked = { signInViewModel.onIntent(SignInIntent.OnSignInWithEmailAndPassword) },
+        onSignInWithGoogleClicked = { signInViewModel.onIntent(SignInIntent.OnSignInWithGoogle(currentContext))},
         modifier
     )
 }
@@ -96,6 +109,7 @@ private fun SignInScreenContent(
     onPasswordVisibilityChange: () -> Unit,
     onSignUpButtonClicked: () -> Unit,
     onSignInButtonClicked: () -> Unit,
+    onSignInWithGoogleClicked : () -> Unit,
     modifier: Modifier = Modifier
 ):Unit{
     Column(
@@ -184,11 +198,17 @@ private fun SignInScreenContent(
                     strokeWidth = 2.dp
                 )
             } else {
-                Text(text = stringResource(R.string.sign_in_with_email), fontSize = 16.sp)
+                Text(
+                    text = stringResource(R.string.sign_in_with_email),
+                    fontFamily = FontFamily.SansSerif,
+                    fontWeight = FontWeight.ExtraBold,
+                    fontSize = 15.sp,
+                    letterSpacing = 0.1.em
+                )
             }
         }
 
-        Spacer(modifier = Modifier.height(24.dp));
+        Spacer(modifier = Modifier.height(16.dp));
 
         TextButton(
             onClick = onSignUpButtonClicked
@@ -196,12 +216,47 @@ private fun SignInScreenContent(
             Text(
                 text = stringResource(R.string.sign_up_text),
                 textAlign = TextAlign.Center,
-                fontSize = 16.sp,
-                color = DarkBlue
+                color = DarkBlue,
+                fontFamily = FontFamily.SansSerif,
+                fontWeight = FontWeight.Bold,
+                fontSize = 15.sp,
+                letterSpacing = 0.1.em
             )
         }
+        Spacer(modifier = Modifier.height(16.dp));
 
-        // TODO: IMPLEMENT GOOGLE SIGN IN FUNCTIONALITY
+        OutlinedButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp),
+            onClick = onSignInWithGoogleClicked,
+            colors = ButtonDefaults.outlinedButtonColors(
+                containerColor = DarkBlue,
+                contentColor = Color.White
+            ),
+            border = BorderStroke(1.dp, DarkBlue)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Image(
+                    modifier = Modifier.size(25.dp),
+                    painter = painterResource(R.drawable.google_logo),
+                    contentScale = ContentScale.Fit,
+                    contentDescription = null
+                )
+
+                Text(
+                    text = stringResource(R.string.sign_with_google_text),
+                    fontFamily = FontFamily.SansSerif,
+                    fontWeight = FontWeight.ExtraBold,
+                    fontSize = 15.sp,
+                    letterSpacing = 0.1.em
+                )
+            }
+        }
     }
 }
 
@@ -215,7 +270,8 @@ private fun SignInScreenPreview():Unit{
             onPasswordChange = {},
             onPasswordVisibilityChange = {},
             onSignUpButtonClicked = {},
-            onSignInButtonClicked = {}
+            onSignInButtonClicked = {},
+            onSignInWithGoogleClicked = {}
         );
     }
 }
