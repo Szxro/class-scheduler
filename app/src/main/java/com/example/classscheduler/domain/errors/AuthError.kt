a2044f7e78407006da37ec67aa76a7e7a0c56ba1
Sebastian <svargas@isf.do>
2025-10-21 15:21:36 -0400

    refactor(domain): update AuthError class

--- app/src/main/java/com/example/classscheduler/domain/errors/AuthError.kt
+++ app/src/main/java/com/example/classscheduler/domain/errors/AuthError.kt
@@ -1,27 +1,28 @@
 package com.example.classscheduler.domain.errors
 
+import com.example.classscheduler.core.ui.UiText
 import com.example.classscheduler.domain.primitives.Error
+import com.example.classscheduler.R;
 
-open class AuthError : Error(){
+open class AuthError(val error: UiText) : Error(error){
 
     // Firebase Auth
-    object UserNotFound : AuthError();
+    object UserNotFound : AuthError(UiText.StringResource(R.string.user_not_found_exception));
 
-    object InvalidEmailOrPassword : AuthError();
+    object InvalidEmailOrPassword : AuthError(UiText.StringResource(R.string.invalid_user_email_or_password_exception));
 
-    object EmailCollision : AuthError();
+    object EmailCollision : AuthError(UiText.StringResource(R.string.email_collision_exception));
 
     // Google OAUTH
-    object CredentialError : AuthError();
 
-    object CredentialsCancellation : AuthError();
+    object CredentialsCancellation : AuthError(UiText.StringResource(R.string.credentials_cancellation_exception));
 
-    object NoCredentialsFound : AuthError();
+    object NoCredentialsFound : AuthError(UiText.StringResource(R.string.no_credentials_found_exception));
 
-    object CredentialsCleanUpError : AuthError();
+    object CredentialsCleanUpError : AuthError(UiText.StringResource(R.string.credentials_clean_up_exception));
 
-    object UnknownCredentials : AuthError();
+    object UnknownCredentials : AuthError(UiText.StringResource(R.string.unknown_credentials_exception));
 
     // Generic Error
-    object UnknownError : AuthError();
+    object UnknownError : AuthError(UiText.StringResource(R.string.generic_exception));
 }
