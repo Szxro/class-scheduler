ce4ff78e94a42fc4c5e1a89ec8f216016e8dc2f3
Sebastian <svargas@isf.do>
2025-10-22 14:16:39 -0400

    refactor(main): add ResetPassword screen to NavHost, use extension function for SnackbarHostState, and fix navigation stack issue

--- app/src/main/java/com/example/classscheduler/MainActivity.kt
+++ app/src/main/java/com/example/classscheduler/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.remember
@@ -16,8 +17,11 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
+import com.example.classscheduler.core.utils.ext.showMessage
 import com.example.classscheduler.ui.home.HomeRoute
 import com.example.classscheduler.ui.home.HomeScreen
+import com.example.classscheduler.ui.resetpassword.ResetPasswordRoute
+import com.example.classscheduler.ui.resetpassword.ResetPasswordScreen
 import com.example.classscheduler.ui.signin.SignInRoute
 import com.example.classscheduler.ui.signin.SignInScreen
 import com.example.classscheduler.ui.signup.SignUpRoute
@@ -33,7 +37,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             val navController = rememberNavController();
-            val scope = rememberCoroutineScope();
+            val scope = rememberCoroutineScope()
             val snackBarHostState = remember { SnackbarHostState() }
 
             ClassSchedulerTheme {
@@ -46,8 +50,9 @@ class MainActivity : ComponentActivity() {
                             SignInScreen(
                                 openHomeScreen = { user ->
                                     navController.navigate(HomeRoute(user.email)){
-                                        popUpTo(SignInRoute){
-                                            inclusive = true; // Remove the SignInRoute from the stack
+                                        // when the user navigate to home is going  to delete the hold stack [SignIn, SignUp,ResetPassword] -> [Home]
+                                        popUpTo(navController.graph.startDestinationId) {
+                                            inclusive = true
                                         }
                                         launchSingleTop = true; // Avoid multiple instances of the same screen
                                     };
@@ -57,38 +62,45 @@ class MainActivity : ComponentActivity() {
                                         launchSingleTop = true;
                                     };
                                 },
-                                showSnackBar = { text ->
-                                    val message = text.asString(this@MainActivity);
-
-                                    scope.launch {
-                                        snackBarHostState.currentSnackbarData?.dismiss();
-
-                                        snackBarHostState.showSnackbar(message);
-                                    };
-                                }
+                                openResetPasswordScreen = {
+                                    navController.navigate(ResetPasswordRoute){
+                                        launchSingleTop = true;
+                                    }
+                                },
+                                showSnackBar = { text ->  snackBarHostState.showMessage(text,this@MainActivity,scope) }
                             )
                         }
                         composable<SignUpRoute>{
                             SignUpScreen(
                                 openHomeScreen = { user ->
                                     navController.navigate(HomeRoute(user.email)){
-                                        popUpTo(SignInRoute){
-                                            inclusive = true;
+                                        popUpTo(navController.graph.startDestinationId) {
+                                            inclusive = true
                                         }
                                         launchSingleTop = true;
                                     };
                                 },
                                 openSignInScreen = {
-                                    navController.navigate(SignInRoute)
+                                    navController.navigate(SignInRoute){
+                                        launchSingleTop = true;
+                                    }
+                                },
+                                showSnackBar = { text ->  snackBarHostState.showMessage(text,this@MainActivity,scope) }
+                            );
+                        }
+                        composable<ResetPasswordRoute>{
+                            ResetPasswordScreen(
+                                openSignInScreen = {
+                                    navController.navigate(SignInRoute){
+                                        launchSingleTop = true;
+                                    }
                                 },
                                 showSnackBar = { text ->
-                                    val message = text.asString(this@MainActivity);
-
-                                    scope.launch {
-                                        snackBarHostState.currentSnackbarData?.dismiss();
-
-                                        snackBarHostState.showSnackbar(message);
-                                    };
+                                    snackBarHostState.showMessage(
+                                        text,
+                                        this@MainActivity,scope,
+                                        SnackbarDuration.Long
+                                    )
                                 }
                             );
                         }
