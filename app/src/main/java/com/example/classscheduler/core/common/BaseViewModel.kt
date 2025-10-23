f0df7b65673782a2f3d20afb4dcd55b11551c13d
Sebastian <svargas@isf.do>
2025-10-23 11:51:41 -0400

    refactor(core): add validator property and function to assign the validator

--- app/src/main/java/com/example/classscheduler/core/common/BaseViewModel.kt
+++ app/src/main/java/com/example/classscheduler/core/common/BaseViewModel.kt
@@ -3,14 +3,21 @@ package com.example.classscheduler.core.common
 import androidx.lifecycle.ViewModel
 import com.example.classscheduler.core.ui.UiIntent
 import com.example.classscheduler.core.ui.UiEvent
+import com.example.classscheduler.core.utils.validation.Validator
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
 
 
-abstract class BaseViewModel<TIntent: UiIntent>: ViewModel() {
-    abstract fun onIntent(intent: TIntent): Unit;
-
+abstract class BaseViewModel<TIntent: UiIntent, TState>: ViewModel() {
     protected val channel: Channel<UiEvent> = Channel();
 
+    protected var validator: Validator<TState>? = null;
+
     val events = channel.receiveAsFlow();
+
+    abstract fun onIntent(intent: TIntent): Unit;
+
+    fun addValidator(validator: Validator<TState>): Unit{
+        this.validator = validator;
+    }
 }
