153990db39986aceb11895a378882c466ab4c911
Sebastian Vargas <sebastian.vargas@minerd.edu.do>
2025-11-02 15:03:48 -0400

    chore(config): add missing implementation of json serialization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,9 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
 
+    // Serialization
+    implementation(libs.kotlinx.serialization.json);
+
     // Firebase
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.firestore)
