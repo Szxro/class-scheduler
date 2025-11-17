6d05adc15cf9a0ed0d906e753e8a5138c6aa950f
Sebastian Vargas <sebastian.vargas@minerd.edu.do>
2025-11-17 15:39:05 -0400

    feat(ui): now the selectable dropdown menu is a controlled component

--- app/src/main/java/com/example/classscheduler/ui/shared/SelectableDropdownMenu.kt
+++ app/src/main/java/com/example/classscheduler/ui/shared/SelectableDropdownMenu.kt
@@ -15,9 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.listSaver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.toMutableStateList
@@ -29,15 +27,12 @@ fun <T> SelectableDropdownMenu(
     label: String = "Select item(s)",
     options: List<T>,
     itemLabel: (T) -> String,
+    selected: List<T>,
     onSelectionChange: (List<T>) -> Unit
 ) {
     var isExpanded by rememberSaveable { mutableStateOf(false) };
 
-    val selectedItems = rememberSaveable(
-        saver = listSaver( // Need to implement a custom saver (list saver) to solve the exception problem
-            save = { it.toList() },
-            restore = { it.toMutableStateList() })
-    ) { mutableStateListOf<T>() }
+    val selectedItems = selected.toMutableStateList();
 
     ExposedDropdownMenuBox(
         expanded = isExpanded,
