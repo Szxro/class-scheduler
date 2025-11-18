e256e036012d87e25e1d5c3d9adc6aeda6c41459
Sebastian Vargas <sebastian.vargas@minerd.edu.do>
2025-11-18 16:07:29 -0400

    feat: add weekly scheduler broadcast receiver

---
+++ app/src/main/java/com/example/classscheduler/data/receivers/WeeklyScheduleBroadCast.kt
@@ -0,0 +1,52 @@
+package com.example.classscheduler.data.receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import com.example.classscheduler.data.services.AlarmSchedulerServiceImpl
+import com.example.classscheduler.data.services.NotificationServiceImpl
+import com.example.classscheduler.domain.models.AlarmItem
+import dagger.hilt.android.AndroidEntryPoint
+import java.time.DayOfWeek
+import java.time.LocalTime
+import javax.inject.Inject
+
+@AndroidEntryPoint // can define more than one entry point
+class WeeklyScheduleBroadCast : BroadcastReceiver() {
+    @Inject
+    lateinit var alarmScheduler: AlarmSchedulerServiceImpl;
+
+    @Inject
+    lateinit var notificationService: NotificationServiceImpl
+
+    override fun onReceive(context: Context, intent: Intent?): Unit {
+        val title = intent?.getStringExtra("title") ?: return;
+        val description = intent.getStringExtra("description") ?: return;
+
+        val localtime = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent.getSerializableExtra("localtime", LocalTime::class.java)
+        } else {
+            @Suppress("DEPRECATION")
+            intent.getSerializableExtra("localtime") as? LocalTime
+        } ?: return;
+
+        val dayOfTheWeekValue = intent.getIntExtra("dayOfTheWeek", 0);
+
+        // Obtaining a day of the week of the given value
+        val dayOfTheWeek = DayOfWeek.of(dayOfTheWeekValue);
+
+        // show notification
+        notificationService.showNotification(title, description);
+
+        // Re-schedule the alarm for the next week
+        val nextWeekAlarmItem = AlarmItem(
+            title,
+            description,
+            localtime,
+            dayOfTheWeek
+        );
+
+        alarmScheduler.scheduleWeeklyAlarm(nextWeekAlarmItem);
+    }
+}
