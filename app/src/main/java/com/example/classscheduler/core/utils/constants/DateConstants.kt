77932c131cc481e7a026071c40e11fed43710a6f
Sebastian Vargas <sebastian.vargas@minerd.edu.do>
2025-11-24 14:06:26 -0400

    feat(core): add date constants object

---
+++ app/src/main/java/com/example/classscheduler/core/utils/constants/DateConstants.kt
@@ -0,0 +1,7 @@
+package com.example.classscheduler.core.utils.constants
+
+object DateConstants {
+    val DAYS_OF_THE_WEEK = listOf(
+        "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+    );
+}
