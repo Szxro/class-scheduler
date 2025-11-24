e49ff77b76fe5af3fafa2d547aabd3a4034edf6e
Sebastian Vargas <sebastian.vargas@minerd.edu.do>
2025-11-24 14:34:37 -0400

    fix: add missing variables to the auth repository interface

--- app/src/main/java/com/example/classscheduler/domain/interfaces/AuthRepository.kt
+++ app/src/main/java/com/example/classscheduler/domain/interfaces/AuthRepository.kt
@@ -2,8 +2,12 @@ package com.example.classscheduler.domain.interfaces
 
 import android.content.Context
 import com.example.classscheduler.domain.primitives.Result
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.coroutines.flow.Flow
 
 interface AuthRepository {
+    val currentUser: FirebaseUser?
+    val currentIdFlow: Flow<String?>
     suspend fun signInWithEmailAndPassword(email: String, password: String): Result<Nothing>;
 
     suspend fun signInWithGoogle(context: Context): Result<Nothing>;
